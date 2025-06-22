46ac72b7fa90e861615943789c990cfc4b1c9b7c
Shetu Srivastav <srivastavashetu@gmail.com>
2025-06-22 14:43:06 +0530

    Updated application.yml

--- src/main/java/com/smartdesk/SmartdeskApplication.java
+++ src/main/java/com/smartdesk/SmartdeskApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmartdeskApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmartdeskApplication.class, args);
+		SpringApplication.run(SmartdeskApplication.class, args); //to run docker-compose up
 	}
 
 }
